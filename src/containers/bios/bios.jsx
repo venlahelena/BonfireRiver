9ea93c7f7cb92cd809491316d791d8c2c1cdc9eb
Venla <venlaj@hotmail.com>
2022-01-21 13:59:35 +0200

    Added google tags

--- src/containers/bios/bios.jsx
+++ src/containers/bios/bios.jsx
@@ -13,7 +13,7 @@ function Bios() {
                 <div className="tuonela__whattuonela-feature">
                     <Bio img="" title="Daniel" text="Daniel is the Environment Artist and Level Designer here in the North. He loves mixing together the successful ingredients of game design like mixing a good drink."/>
                     <Bio img="" title="Gabriel" text="Gabriel is the Sound Designer and Cinematographer of Bonfire River Studios. His specialties include sound effect production, video editing, graphic design and musical composition."/>
-                    <Bio img="" title="June" text="June happily holds the title of Concept Artist here at Bonfire River Studios. They made those beautiful concepts on our social media. You can bet we're proud."/>
+                    <Bio img="" title="June" text="Juniper happily holds the title of Concept Artist here at Bonfire River Studios. They're passionate about storytelling and making art make sense."/>
                     <Bio img="" title="Venla" text="Venla is responsible of programming and character desing at Bonfire River Studios. "/>
                 </div>
             </div>
